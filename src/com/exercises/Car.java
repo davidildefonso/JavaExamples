a5c880196c9279ddca1ba607ec7c63f636a7a466
David Ildefonso <david_lion_ig@outlook.com.pe>
2021-07-27 17:11:35 -0500

    cont

---
+++ src/com/exercises/Car.java
@@ -0,0 +1,20 @@
+package com.exercises;
+
+public class Car {
+    String make = "chevrolet";
+    String model = "corvette";
+    int year = 2004;
+    String color = "blue";
+    double price = 12999.99;
+
+    void drive(){
+        System.out.println("You drive" +
+                " the car");
+    }
+
+    void brake(){
+        System.out.println("You step on" +
+                " the brakes.");
+    }
+
+}
--- src/com/exercises/Main.java
+++ src/com/exercises/Main.java
@@ -35,10 +35,58 @@ public class Main {
         animals.add("rat");
         animals.add("bird");
 
-        for (String animal : animals) {
-            System.out.println(animal + " ");
-        }
+        //for (String animal : animals) {
+           // System.out.println(animal + " ");
+        //}
 
 
+        /*
+        hello("David");
+        System.out.println(sumOfTwoNumbers(3.1413, 6.553));
+        System.out.println(sumOfTwoNumbers(3,12));
+
+        System.out.printf("This is a format string %s \n", "idiot");
+        System.out.printf("hola numero: %d", 101);
+        System.out.println();
+        System.out.printf("boolean %b", true);
+        System.out.println();
+        System.out.printf("character: %c", 'X');
+        System.out.println();
+        System.out.printf("float number %f", 6.2232);
+        System.out.println();
+        System.out.printf("width example: %5d %5d", 23, 12);
+        System.out.println();
+
+        System.out.printf("precision example: %.2f", 6.442212);
+        System.out.println();
+
+        System.out.printf("flags example : %20.2f", 11200223.1415);
+*/
+
+        Car myCar = new Car();
+        System.out.println(myCar.color);
+        myCar.drive();
+        myCar.brake();
+
+        Car secondCar = new Car();
+        System.out.println(myCar.model);
+        secondCar.drive();
+        secondCar.brake();
+
+
+
+
+    }
+
+    static double sumOfTwoNumbers(double num1, double num2){
+        return num1 + num2;
+    }
+
+    static int sumOfTwoNumbers(int num1,int num2){
+        return num1 + num2;
+    }
+
+    static void hello(String name){
+        System.out.println("Hello "+ name +"! ");
     }
 }
